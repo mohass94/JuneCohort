592c1efa1339f90c32655a4677014dbf784b27cf
mohamed hassan <Mohass@mohameds-MBP-3.lan>
2023-07-26 17:27:56 -0700

    Lesson1 edits

---
+++ Javascript/lessons/lesson1.js
@@ -0,0 +1,14 @@
+//this is a single line comment
+
+/*
+This
+will
+be 
+multi-line 
+comment*/
+
+console.log("Hello you have executed lesson 1 file.");
+
+console.log("Hello world");
+
+var name = "Mohass";
