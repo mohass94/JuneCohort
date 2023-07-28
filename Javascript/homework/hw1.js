305d56d5d2fafa5f48aff89f034b5128c47c01b4
mohamed hassan <Mohass@mohameds-MBP-3.lan>
2023-07-27 23:16:45 -0700

    Hw1 javascript

---
+++ Javascript/homework/hw1.js
@@ -0,0 +1,30 @@
+// q.1 Using string concatenation add two different strings together and print this off.
+
+console.log("Hello " + "World!")
+
+var firstWord = "Hello"
+var secondWord = "World!"
+
+//Another way of doing it.
+console.log(firstWord + " " + secondWord)
+
+
+
+//q.2 Using variables create a variable for a number, a string. Then print each of these variables.
+
+var firstWord = "Hello"
+var secondWord = 2
+
+console.log(firstWord)
+console.log(secondWord)  
+
+
+/*q.3 Using string concatenation in javascript combine 3 different variables and prints it off. 
+I.e variable for name age and date of birth will result in “my name is abdulkadir I am 24 years old and I was born in 1998”. 
+(you cannot use name age and dob as your variables). I am somali I was born in qatar I live in San Diego*/
+
+var ethnicity = "Somali"
+var birthplace = "Qatar"
+var home = "San Diego"
+
+console.log("I am" , ethnicity, "I was born in" , birthplace, "I live in", home,)
